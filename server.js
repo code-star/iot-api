a88710092ebb0034316d6f4eff593453ade80628
M.D. van Es <es@mdworld.nl>
2020-11-06 13:53:50 +0100

    initial

---
+++ server.js
@@ -0,0 +1,28 @@
+// Run with `yarn start`, it will start `node index.js`
+const express = require("express");
+const bodyParser = require('body-parser')
+const app = express();
+const jsonParser = bodyParser.json();
+
+const port = 3000;
+
+const memstorage = [];
+
+app.get("/get", (req, res) => {
+  res.send(memstorage);
+});
+
+app.post("/set", jsonParser, (req, res) => {
+  // Test with: curl --request POST -d '{ "foo": "bar" }' -H "Content-Type: application/json" http://localhost:3000/set
+  if (req.body) {
+    memstorage.push(req.body);
+    console.log("memstorage is now:", memstorage);
+    res.send("data stored");
+  } else {
+    res.status(500).send("failed to store data");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`IoT API listening at http://localhost:${port}`);
+});
